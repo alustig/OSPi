1d2900fd1aec11a2b92ad030cc2bd84fb4a3613f
Adam Lustig <alustig@gmail.com>
2014-11-03 05:35:49 -0500

    dev

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
+import ephem
 from pyzipcode import ZipCodeDatabase
+
 zcdb = ZipCodeDatabase()
 zipcode = zcdb[23229]
 print zipcode.zip
 print zipcode.city
 print zipcode.state
-print zipcode.longitude
 print zipcode.latitude
+print zipcode.longitude
 print zipcode.timezone
+
+o=ephem.Observer()
+o.lat=zipcode.latitude
+o.long=zipcode.longitude
+s=ephem.Sun()
+s.compute()
+print "Rising ",ephem.localtime(o.next_rising(s))
+print "Setting ",ephem.localtime(o.next_setting(s))
