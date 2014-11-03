e762fd4946548eaf58948724bb2a1a1c3f06e446
Adam Lustig <alustig@gmail.com>
2014-11-03 05:31:02 -0500

    dev

---
+++ test.py
@@ -0,0 +1,9 @@
+from pyzipcode import ZipCodeDatabase
+zcdb = ZipCodeDatabase()
+zipcode = zcdb[23229]
+zipcode.zip
+zipcode.city
+zipcode.state
+zipcode.longitude
+zipcode.latitude
+zipcode.timezone
