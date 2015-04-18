1e0c9d230b55f27a36decc69d97623ea2ee9eade
alustig <alustig@gmail.com>
2015-04-18 07:06:12 -0400

    bugs

--- plugins/sunrise_sunset.py
+++ plugins/sunrise_sunset.py
@@ -171,7 +171,7 @@ def create_program(data):
         sse = map(int, sse)
         if ss[1] > sse[1]:
             ssm = 60+ss[1]
-            sse[0] -= 1
+            ss[0] += 1
         else:
             ssm = sse[1]-ss[1]
         sshr = sse[0]-ss[0]
